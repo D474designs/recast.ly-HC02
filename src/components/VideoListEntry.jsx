b893e024fa09c45dc2699a26a7ae9ca2545a61ff
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-08-07 16:32:44 -0500

    add rebuilt code to rebuilt init

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,5 +1,13 @@
 var VideoListEntry = () => {
-
+  <div className="video-list-entry">
+    <div className="media-left media-middle">
+      <img className="media-object" src="" alt="" />
+    </div>
+    <div className="media-body">
+      <div className="video-list-entry-title"></div>
+      <div className="video-list-entry-detail"></div>
+    </div>
+  </div>
 };
 
 VideoListEntry.propTypes = {
